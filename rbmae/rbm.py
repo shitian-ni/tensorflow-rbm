aad600b8af0f1ef3fa33c067077cab9d17d6b867
Egor Malykh <fnk@fea.st>
2016-08-05 00:40:10 +0300

    implemented base rbm and bbrbm

--- rbmae/rbm.py
+++ rbmae/rbm.py
@@ -4,50 +4,73 @@ from .util import np_xavier_init
 
 
 class RBM:
-    def __init__(self, n_visible, n_hidden, *, learning_rate=0.1, momentum=0.5):
+    def __init__(self, n_visible, n_hidden, *, learning_rate, momentum):
         self.n_visible = n_visible
         self.n_hidden = n_hidden
-        self.alpha = learning_rate
+        self.learning_rate = learning_rate
         self.momentum = momentum
 
-        self._initialize_weights()
-
         self.x = tf.placeholder(tf.float32, [None, self.n_visible])
+        self.hidden_placeholder = tf.placeholder(tf.float32, [None, self.n_hidden])
         self.w = tf.placeholder(tf.float32, [self.n_visible, self.n_hidden])
         self.visible_bias = tf.placeholder(tf.float32, [self.n_visible])
         self.hidden_bias = tf.placeholder(tf.float32, [self.n_hidden])
 
-    def _initialize_weights(self):
-        self.curr_w = np_xavier_init(self.n_visible, self.n_hidden, const=1.0, dtype=np.float32)
-        self.curr_visible_bias = np.zeros((self.n_visible,), dtype=tf.float32)
-        self.curr_hidden_bias = np.zeros((self.n_hidden,), dtype=tf.float32)
+        self.visible_p = None
+        self.update_w = None
+        self.update_visible_bias = None
+        self.update_hidden_bias = None
 
-    def compute_err(self):
+        self._initialize_vars()
+
+        self.compute_hidden = tf.nn.sigmoid(tf.matmul(self.x, self.w) + self.hidden_bias)
+        self.compute_visible = tf.nn.sigmoid(tf.matmul(self.compute_hidden, tf.transpose(self.w)) + self.visible_bias)
+        self.compute_err = tf.reduce_mean(tf.square(self.x - self.visible_p))
+
+        init = tf.initialize_all_variables()
+        self.sess = tf.Session()
+        self.sess.run(init)
+
+    def _initialize_vars(self):
         pass
+
+    def compute_err(self, batch_x):
+        return self.sess.run(self.compute_err, feed_dict={self.x: batch_x})
 
     def compute_free_energy(self):
         pass
 
-    def transform(self):
-        pass
+    def transform(self, batch_x):
+        return self.sess.run(self.compute_hidden, feed_dict={self.x: batch_x})
 
-    def reconstruct(self):
-        pass
+    def reconstruct(self, batch_x):
+        return self.sess.run(self.compute_visible, feed_dict={self.x: batch_x})
 
-    def partial_fit(self):
-        pass
+    def partial_fit(self, batch_x):
+        self.sess.run([self.update_w, self.update_visible_bias, self.update_hidden_bias],
+                      feed_dict={self.x: batch_x})
+
+        return self.sess.run(self.compute_err, feed_dict={self.x: batch_x})
 
     def get_weights(self):
-        pass
+        return self.w.eval(self.sess), \
+               self.visible_bias.eval(self.sess),\
+               self.hidden_bias.eval(self.sess)
 
-    def get_weights_np(self):
-        pass
+    def save_weights(self, path, weights_names):
+        saver = tf.train.Saver({weights_names[0]: self.w,
+                                weights_names[1]: self.visible_bias,
+                                weights_names[2]: self.hidden_bias})
+        return saver.save(self.sess, path)
 
-    def save_weights(self):
-        pass
+    def set_weights(self, w, visible_bias, hidden_bias):
+        self.sess.run(self.w.assign(w))
+        self.sess.run(self.visible_bias.assign(visible_bias))
+        self.sess.run(self.hidden_bias.assign(hidden_bias))
 
-    def set_weights(self):
-        pass
+    def load_weights(self, path, weights_names):
+        saver = tf.train.Saver({weights_names[0]: self.w,
+                                weights_names[1]: self.visible_bias,
+                                weights_names[2]: self.hidden_bias})
+        saver.restore(self.sess, path)
 
-    def load_weights(self):
-        pass
