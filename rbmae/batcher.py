acb0c448e59752a8362c646dfc7bb742ae09b016
Egor Malykh <fnk@fea.st>
2016-08-12 14:47:14 +0400

    new realization of batcher (not tested yet)

--- rbmae/batcher.py
+++ rbmae/batcher.py
@@ -1,10 +1,48 @@
 import numpy as np
 
-
 class Batcher:
-    def __init__(self, data, labels=None, *, batch_size=100):
-        assert 0 < len(data.shape) < 3
-        assert 0 < len(labels.shape) < 3
+    def __init__(*args, batch_size=100):
+        if len(args) == 0:
+            raise ValueError('Batcher must take at least one argument')
+
+        for typecheck in  map(lambda x: type(x) == np.ndarray, args):
+            if not typecheck:
+                raise TypeError('Type of all arguments must be np.ndarray')
+
+        n_ = args[0].shape
+        for n in map(lambda x: x.shape[0], args[1:]):
+            if n != n_:
+                raise ValueError('Shape[0] must be the same for all arguments')
+
+        self.batch_size = batch_size
+
+        self._n = n_
+        self._args_views = args
+        self._indices = np.arange(self._n)
+        self._ptr = 0
+
+        def reset(self):
+            self._ptr = 0
+
+        def shuffle(self):
+            np.random.shufle(self._indices)
+            self._args_views = list(map(lambda x: x[self._indices], self._args_views))
+
+        @property
+        def has_next_batch(self):
+            return self._ptr < self._n
+
+        @property
+        def next_batch(self):
+            start = self._ptr
+            self._ptr += self.batch_size
+            end = self._ptr
+            return tuple(map(lambda x: x[start:end], self._args_view))
+
+
+
+class Batcher2:
+    def __init__(self, data, labels, *, batch_size=100):
         assert data.shape[0] == labels.shape[0]
 
         self.batch_size = batch_size
