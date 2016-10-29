11bafb8a75d5cb9580b645352575d29911fbc428
Egor Malykh <fnk@fea.st>
2016-10-29 14:44:53 +0300

    fit function && BBRBM example

--- README.md
+++ README.md
@@ -11,6 +11,65 @@ This is a fork of https://github.com/Cospel/rbm-ae-tf with some corrections and 
 - using probabilities instead of samples for training
 - implemented both Bernoulli-Bernoulli RBM and Gaussian-Bernoulli RBM
 
+### BBRBM Example
+Bernoulli-Bernoulli RBM is good for Bernoulli-distributed binary input data. MNIST, for example.
+
+Load data and train RBM:
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+from tfrbm import BBRBM, GBRBM
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist_images = mnist.train.images
+
+bbrbm = BBRBM(n_visible=784, n_hidden=64, learning_rate=0.1, momentum=0.95)
+errs = bbrbm.fit(mnist_images, n_epoches=20, batch_size=128, tqdm='notebook')
+plt.plot(errs)
+plt.show()
+```
+
+Output:
+```
+Epoch: 0, error: 0.079566
+Epoch: 1, error: 0.047970
+Epoch: 2, error: 0.040229
+Epoch: 3, error: 0.036163
+Epoch: 4, error: 0.033523
+Epoch: 5, error: 0.031571
+Epoch: 6, error: 0.030071
+Epoch: 7, error: 0.028901
+Epoch: 8, error: 0.027961
+Epoch: 9, error: 0.027207
+```
+
+![Error plot](https://habrastorage.org/files/861/c6d/f0d/861c6df0d1604e49b4a710d7f0828cbf.png)
+
+Examine some reconstructed data:
+```python
+IMAGE = 1
+
+def show_digit(x):
+    plt.imshow(x.reshape((28, 28)), cmap=plt.cm.gray)
+    plt.show()
+
+image = mnist_images[IMAGE]
+show_digit(image)
+image_rec = bbrbm.reconstruct(image.reshape(1,-1))
+show_digit(image_rec)
+```
+
+Examples:
+
+![3 original](https://habrastorage.org/files/fa2/a3e/35b/fa2a3e35b2cd417fa70de3e6aa146464.png)
+
+![3 reconstructed](https://habrastorage.org/files/eb3/d8b/8c8/eb3d8b8c8ddb48a384f4ddd43a5ef155.png)
+
+![4 original](https://habrastorage.org/files/1c8/2ba/0e9/1c82ba0e906f4cb49ec6fa0e2e5bfafe.png)
+
+![4 reconstructed](https://habrastorage.org/files/cbb/e65/5ff/cbbe655ff66049348418991b8084088a.png)
+
 ### Original README
 
 Tensorflow implementation of Restricted Boltzman Machine and Autoencoder for layerwise pretraining of Deep Autoencoders with RBM. Idea is to first create RBMs for pretraining weights for autoencoder. Then weigts for autoencoder are loaded and autoencoder is trained again. In this implementation you can also use tied weights for autoencoder(that means that encoding and decoding layers have same transposed weights!).
--- tfrbm/rbm.py
+++ tfrbm/rbm.py
@@ -7,7 +7,7 @@ class RBM:
     def __init__(self, n_visible, n_hidden, learning_rate=1.0, momentum=1.0, xavier_const=1.0, err_function='mse'):
         assert 0.0 <= momentum <= 1.0
         assert err_function == 'mse' or err_function == 'cosine'
-        
+
         self.n_visible = n_visible
         self.n_hidden = n_hidden
         self.learning_rate = learning_rate
@@ -37,7 +37,7 @@ class RBM:
         assert self.compute_hidden is not None
         assert self.compute_visible is not None
         assert self.compute_visible_from_hidden is not None
-        
+
         if err_function == 'cosine':
             x1_norm = tf.nn.l2_normalize(self.x, 1)
             x2_norm = tf.nn.l2_normalize(self.compute_visible, 1)
@@ -71,10 +71,65 @@ class RBM:
     def partial_fit(self, batch_x):
         self.sess.run(self.update_weights + self.update_deltas, feed_dict={self.x: batch_x})
 
+    def fit(self, data_x, n_epoches=10, batch_size=128, shuffle=True, verbose=True, tqdm=None):
+        n_data = data_x.shape[0]
+        n_batches = n_data // batch_size + (0 if n_data % batch_size == 0 else 1)
+
+        assert n_batches > 0
+
+        if shuffle:
+            data_x_cpy = data_x.copy()
+        else:
+            data_x_cpy = data_x
+
+        if shuffle:
+            inds = np.arange(n_data)
+
+        if tqdm:
+            if tqdm == 'notebook':
+                from tqdm import tqdm_notebook as tq
+            else:
+                from tqdm import tqdm as tq
+
+        epoches_range = range(n_epoches)
+
+        if tqdm:
+            epoches_range = tq(epoches_range, desc='Epoch')
+
+        errs = []
+
+        for e in epoches_range:
+            epoch_errs = np.zeros((n_batches,))
+            epoch_errs_ptr = 0
+
+            if shuffle:
+                np.random.shuffle(inds)
+                data_x_cpy = data_x_cpy[inds]
+
+            batches_range = range(n_batches)
+
+            if tqdm:
+                batches_range = tq(batches_range, desc='Batch')
+
+            for b in batches_range:
+                batch_x = data_x_cpy[b * batch_size:(b + 1) * batch_size]
+                self.partial_fit(batch_x)
+                batch_err = self.get_err(batch_x)
+                epoch_errs[epoch_errs_ptr] = batch_err
+                epoch_errs_ptr += 1
+
+            if verbose:
+                err_mean = epoch_errs.mean()
+                print('Epoch: {:d}, error: {:4f}'.format(e, err_mean))
+
+            errs = np.hstack([errs, epoch_errs])
+
+        return errs
+
     def get_weights(self):
-        return self.sess.run(self.w), \
-               self.sess.run(self.visible_bias),\
-               self.sess.run(self.hidden_bias)
+        return self.sess.run(self.w),\
+            self.sess.run(self.visible_bias),\
+            self.sess.run(self.hidden_bias)
 
     def save_weights(self, path, weights_names):
         saver = tf.train.Saver({weights_names[0]: self.w,
